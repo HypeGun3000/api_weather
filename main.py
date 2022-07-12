40cd9b12533b19b99904cc91b02aec4460e9adc7
Daniil <81660697+HypeGun3000@users.noreply.github.com>
2022-07-12 18:54:43 +0300

    first fix issues

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import requests
 
-req = requests.get('https://wttr.in/london?nTqm')
-print(req.text)
 
-req = requests.get('https://wttr.in/svo?nTqm')
-print(req.text)
+def main():
+    payloud = {
+        'nTqm': '',
+        'lang': 'ru',
+    }
 
-req = requests.get('https://wttr.in/Череповец?nTqm&lang=ru')
-print(req.text)
+    list_of_weather_places = ['https://wttr.in/london',
+                              'https://wtt1r.in/svo',
+                              'https://wttr.in/Череповец']
+
+    for weather_place in list_of_weather_places:
+        try:
+            response = requests.get(weather_place, params=payloud)
+            print(response.text)
+        except:
+            raise ConnectionError(f'Ошибка подключения!')
+
+
+if __name__ == '__main__':
+    main()
